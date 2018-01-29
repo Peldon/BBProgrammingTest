b6eae21b3658a1e2cf2503bccec265a2958641be
Gerald Schenke <realazrael@gmx.de>
2018-01-29 17:29:53 +0100

    added generic repeater

---
+++ BlueByteProgrammingTest/Repeater.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Decorator.h"
+class Repeater : public Decorator {
+public:
+    Status Execute(Entity* e, Blackboard* b) override {
+        if (child) {
+            Status childStatus;
+            while (true) {
+                childStatus = child->Execute(e, b);
+                if (AbortCondition(childStatus, b)) {
+                    break;
+                }
+            }
+            return childStatus;
+        }
+        return Status::FAILURE;
+    };
+
+    /* override this to implement the condition for the repeater
+     * return true to stop repeating, false for keep repeating
+     */
+    virtual bool AbortCondition(Status s, Blackboard* b) = 0;
+};
+
